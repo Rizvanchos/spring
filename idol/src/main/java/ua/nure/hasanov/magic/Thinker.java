d6e3b2075e7a3b24ede4d97cc8581f326a388524
Rizvan Hasanov <Rizvan_Hasanov@epam.com>
2016-08-29 12:40:34 +0300

    Add aop around example

---
+++ idol/src/main/java/ua/nure/hasanov/magic/Thinker.java
@@ -0,0 +1,5 @@
+package ua.nure.hasanov.magic;
+
+public interface Thinker {
+    void think(String thoughts);
+}
---
+++ idol/src/main/java/ua/nure/hasanov/magic/Volunteer.java
@@ -0,0 +1,18 @@
+package ua.nure.hasanov.magic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Volunteer implements Thinker{
+
+    private String thoughts;
+
+    @Override
+    public void think(String thoughts) {
+        this.thoughts = thoughts;
+    }
+
+    public String getThoughts(){
+        return thoughts;
+    }
+}
