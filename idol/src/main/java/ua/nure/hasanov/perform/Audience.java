d1bf71c9c20f47b772deba0e13639270f4c631b0
Rizvan Hasanov <rizvanchos@mail.ru>
2016-09-01 11:52:36 +0300

    Add volunteer

--- idol/src/main/java/ua/nure/hasanov/perform/Audience.java
+++ idol/src/main/java/ua/nure/hasanov/perform/Audience.java
@@ -1,27 +1,38 @@
 package ua.nure.hasanov.perform;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
 public class Audience {
 
+    @Pointcut("execution(* ua.nure.hasanov.perform.Performable.perform(..))")
+    public void performance() {
+    }
+
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("The audience is taking their seats.");
     }
 
+    @Before("performance()")
     public void turnOffCellPhones() {
         System.out.println("The audience is turning off their cellphones");
     }
 
+    @AfterReturning("performance()")
     public void applaud() {
         System.out.println("CLAP CLAP CLAP CLAP CLAP");
     }
 
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Boo! We want our money back!");
     }
 
+    @Around("performance()")
     public void watchPerformance(ProceedingJoinPoint joinPoint) {
         try {
             takeSeats();
