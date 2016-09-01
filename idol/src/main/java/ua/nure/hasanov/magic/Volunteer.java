f08654683ab978de08144bb9337bab202f27a9e7
Rizvan Hasanov <rizvanchos@mail.ru>
2016-09-01 11:52:36 +0300

    Add volunteer

--- idol/src/main/java/ua/nure/hasanov/magic/Volunteer.java
+++ idol/src/main/java/ua/nure/hasanov/magic/Volunteer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class Volunteer implements Thinker{
 
     private String thoughts;
-
+    
     @Override
     public void think(String thoughts) {
         this.thoughts = thoughts;
